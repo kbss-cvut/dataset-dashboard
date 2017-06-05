e1649a061be84c43d8a31f3120f053f2a7f0467f
Michal Med <michal.med.jr@gmail.com>
2017-06-05 15:27:09 +0200

    spatial widget update

--- src/main/webapp/js/components/dashboard/widgets/spatial-widget/SpatialWidget.js
+++ src/main/webapp/js/components/dashboard/widgets/spatial-widget/SpatialWidget.js
@@ -7,7 +7,16 @@ import {TreeNode} from "rc-tree";
 import LoadingWrapper from "../../../misc/LoadingWrapper";
 import {render} from "react-dom";
 import {Map, Marker, Popup, TileLayer} from "react-leaflet";
+import proj4 from "proj4";
+import ClusterLayer from 'react-leaflet-cluster-layer';
 
+// import {
+//     interaction, layer, custom, control, //name spaces
+//     Interactions, Overlays, Controls,     //group
+//     Map, Layers, Overlay, Util    //objects
+// } from "react-openlayers";
+
+proj4.defs("http://www.opengis.net/def/crs/EPSG/0/5514","+proj=krovak +lat_0=49.5 +lon_0=24.83333333333333 +alpha=30.28813972222222 +k=0.9999 +x_0=0 +y_0=0 +ellps=bessel +towgs84=589,76,480,0,0,0,0 +units=m +no_defs");
 
 class SpatialWidget extends React.Component {
     constructor(props) {
@@ -58,43 +67,69 @@ class SpatialWidget extends React.Component {
         // parse it from xml to numbers,
         // attach to id,
 
-        var points;
+        var points = [];
         var data = this.state.data;
         var i = 0;
         data.forEach((point) => {
-            points[i]['coords'] = point["http://www.opengis.net/ont/geosparql#asGML"][0]['@value'];
-            points[i]['id'] = point["http://www.opengis.net/ont/geosparql#asGML"][0]['@value'];
+            // vymysli lepsi dadovy model
             var xmlDoc;
             if (window.DOMParser){
                 var parser = new DOMParser();
-                xmlDoc = parser.parseFromString(points[i]['coords']);
+                xmlDoc = parser.parseFromString(point["http://www.opengis.net/ont/geosparql#asGML"][0]['@value'],'text/xml');
             }
             else // Internet Explorer
             {
                 xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
                 xmlDoc.async=false;
-                xmlDoc.loadXML(points[i]['coords']);
+                xmlDoc.loadXML(point["http://www.opengis.net/ont/geosparql#asGML"][0]['@value']);
             }
-            points[i]['srs']=xmlDoc.getElementsByTagName("gml:Point")[0].getAttributeByName('srsName')[0].nodeValue;
-            console.log(points[i]['srs']);
-            i++;
+            var coords = xmlDoc.getElementsByTagName("Point")[0].getElementsByTagName('pos')[0].textContent;
+            var lng = Number(coords.split(' ')[0]);
+            var lat = Number(coords.split(' ')[1]);
+            if (xmlDoc.getElementsByTagName("Point")[0].getAttribute('srsName') == "http://www.opengis.net/def/crs/EPSG/0/5514"){
+                let pos = [lng, lat];
+                pos = proj4('http://www.opengis.net/def/crs/EPSG/0/5514', 'EPSG:4326', pos);
+                lng = pos[0];
+                lat = pos[1];
+            }
+            points.push({
+                id    :   point["http://www.opengis.net/ont/gml#id"][0]['@value'],
+                position: {lng: lng, lat: lat},
+                name  :   point["http://schema.org/name"][0]['@value']
+            });
+
+
         });
+
+        // transformace souradnic do epsg 3857
+        //console.log(points[0].crs);
         // calculate mean coordinates as position,
         // render coords with id as label
-        const position = [49.0, 14.5];
+        // set srs to srs
+        //const position = [49.0, 14.5];
+        let position = points[0].position;
+        // let popup = points[0].id;
+
+        let markers = [];
+        points.forEach((point) => {
+            markers.push(
+            <Marker key={point.id} position={point.position}>
+                <Popup>
+                    <span>{point.name}</span>
+                </Popup>
+            </Marker>);
+
+        });
 
         return (
-            <Map center={position} zoom={13} style={{height: 500}}>
+            <Map center={position} zoom={11} style={{height:500 }}>
                 <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
                 />
-                <Marker position={position}>
-                    <Popup>
-                        <span>A pretty CSS3 popup. <br/> Easily customizable.</span>
-                    </Popup>
-                </Marker>
+                {markers}
             </Map>
+
         );
     }
 }
