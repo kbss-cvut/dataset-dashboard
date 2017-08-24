5084215d7eee1fdc9027369c8ee0a642e9f0cac4
Petr Kremen <kremep1@fel.cvut.cz>
2017-08-24 23:13:10 +0200

    [UPD] schema widget - weights, code improvements for vocabularies, decomposition of DatasetSourceTree

--- src/main/webapp/js/components/dashboard/DatasetSourceTree.js
+++ src/main/webapp/js/components/dashboard/DatasetSourceTree.js
@@ -3,20 +3,17 @@
 import React from "react";
 import DatasetSourceStore from "../../stores/DatasetSourceStore";
 import Actions from "../../actions/Actions";
-import {FormControl, Panel} from "react-bootstrap";
+import {Panel} from "react-bootstrap";
 import LoadingWrapper from "../misc/LoadingWrapper";
-import Tree, {SHOW_PARENT, TreeNode} from "rc-tree";
 import DatasetSourceLink from "./DatasetSourceLink";
-import { Scrollbars } from 'react-custom-scrollbars';
+import FilterableTree from '../misc/FilterableTree';
 
-class DatasetSourceTree extends React.Component {
+class DatasetSourceTree extends FilterableTree {
 
     constructor(props) {
         super(props);
         this.state = {
-            datasetSources: [],
-            inputValue: '',
-            sel: '',
+            data: [],
         };
     };
 
@@ -34,7 +31,7 @@ class DatasetSourceTree extends React.Component {
         if (data.action == Actions.getAllDatasetSources) {
             this.props.loadingOff();
             this.setState({
-                datasetSources: data.datasetSources,
+                data: data.datasetSources,
             });
         }
 
@@ -49,99 +46,20 @@ class DatasetSourceTree extends React.Component {
         this.unsubscribe();
     };
 
-
-    // filterTreeNode(dsTree,treeNode) {
-    //     if (this.state.inputValue && treeNode.props.eventKey.match(".*"+this.state.inputValue+".*")) {
-    //         console.log("TRUE")
-    //         return true;
-    //     }
-    //     console.log("FALSE")
-    //     return false;
-    // };
-
-    filterFn(key) {
-        if (!this.state.inputValue || key.indexOf(this.state.inputValue) > -1) {
-            return true;
-        }
-
-        return false;
-    }
-
-    filterTreeNode(tree, node) {
-        return this.state.inputValue && this.filterFn(node.props.eventKey);
-    }
-
-    onChange(e) {
-        this.filterKeys = [];
-        this.setState({
-            inputValue: e.target.value
-        });
-    }
-
     render() {
-        const loop = data => {
-            let c = false;
-            let nodes = [];
-            data.forEach((item) => {
-                const key = item.endpointUrl + " " + item.graphId;
-                const title = <DatasetSourceLink datasetSource={item}/>;
-                let currentC = false;
-                if (this.filterFn(key)) {
-                    currentC = true;
-                }
-                let parts = [];
-                if (item.parts) {
-                    let result = loop(item.parts);
-                    currentC = currentC || result.childMatch;
-                    parts = result.data;
-                }
-                c = c || currentC;
-                if (currentC) {
-                    if (parts.length > 0) {
-                        nodes.push(
-                            <TreeNode
-                                key={key}
-                                title={title}
-                                isLeaf={parts.length == 0}>{parts}
-                            </TreeNode>);
-                    } else {
-                        nodes.push(<TreeNode
-                            key={key}
-                            title={title}
-                            isLeaf={true}
-                        />);
-                    }
-                }
-            });
-            return {childMatch: c, data: nodes};
-        };
-
-        let tree = loop(this.state.datasetSources).data;
-
-        const header = <div>Filter Dataset Source {this.state.inputValue ? ("(" + tree.length) + ")" : ""}</div>;
-
+        const header = <div>Filter Dataset Source</div>;
         return (<Panel header={header}>
-            <FormControl
-                type="text"
-                value={this.state.inputValue}
-                placeholder="Filter ..."
-                onChange={(e) => this.onChange(e)}
+            <FilterableTree
+                data={this.state.data}
+                height={400}
+                selectable={false}
+                expandable={true}
+                showIcon={false}
+                treeCheckable={false}
+                createKey={(item) => item.endpointUrl + " " + item.graphId}
+                createView={(item) => <DatasetSourceLink datasetSource={item}/>}
             />
-            <div>
-                <Scrollbars style={{ height: 400 }}>
-                    <Tree
-                        filterTreeNode={(tree, node) => this.filterTreeNode(this, tree, node)}
-                        selectable={false}
-                        expandable={true}
-                        notFoundContent="No data receivedX."
-                        showIcon={false}
-                        treeCheckable={false}>
-                        {tree}
-                    </Tree>
-                </Scrollbars>
-            </div>
-        </Panel>)
-
+        </Panel>);
     }
 }
 export default LoadingWrapper(DatasetSourceTree, {maskClass: 'mask-container'});
--- src/main/webapp/js/components/dashboard/widgets/schema-widget/SchemaWidget.js
+++ src/main/webapp/js/components/dashboard/widgets/schema-widget/SchemaWidget.js
@@ -2,15 +2,22 @@
 
 import React from "react";
 import Graph from "react-graph-vis";
+import {Checkbox} from "react-bootstrap";
+import Slider from 'react-rangeslider'
+
+import LoadingWrapper from "../../../misc/LoadingWrapper";
 import Actions from "../../../../actions/Actions";
 import DatasetSourceStore from "../../../../stores/DatasetSourceStore";
 import NamespaceStore from "../../../../stores/NamespaceStore";
-import LoadingWrapper from "../../../misc/LoadingWrapper";
-import {Checkbox} from "react-bootstrap";
+
+import Utils from '../../Utils'
+import Rdf from '../../../../vocabulary/Rdf'
+
+const DD_NS = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/";
 
 const nodeTemplate = {
     size: 150,
-    color: "#FFCFCF",
+    color: "#f3ffc2",
     shape: 'box',
     font: {
         face: 'monospace',
@@ -34,7 +41,6 @@ const graphOptions = {
             levelSeparation: 400,
             nodeSpacing: 100,
             treeSpacing: 200,
-
         }
     },
     edges: {
@@ -42,6 +48,17 @@ const graphOptions = {
     }
 };
 
+
+// i - index of the edge
+// max - number of edges
+function getRoundnessForIthEdge(i, max) {
+    return ( ( i == 0 ) ? i : ( ( i % max ) * ( ( i % 2 ) - 0.5 ) / max ) )
+};
+
+function isDataType(uri) {
+    return uri.startsWith('http://www.w3.org/2001/XMLSchema#')
+        || uri.startsWith(Rdf.NS+'langString');
+};
 
 class SchemaWidget extends React.Component {
 
@@ -51,7 +68,22 @@ class SchemaWidget extends React.Component {
             datasetSource: null,
             datasetSchema: null,
             namespaces: {},
-            showAttributes: true
+            /**
+             * Whether to show attributes in nodes
+             */
+            showAttributes: true,
+            /**
+             * Whether to show weight of edges
+             */
+            showWeight: true,
+            /**
+             * Minimal weight - edges below this weight will be discarded from the graph
+             */
+            minWeight: 0,
+            /**
+             * Maximal Limit Weight - computed as a maximum of edge frequencies.
+             */
+            maxLimitWeight: 0,
         };
     };
 
@@ -63,8 +95,19 @@ class SchemaWidget extends React.Component {
         this.unsubscribe();
     };
 
+    computeMax(data) {
+        let max = 0;
+        data.forEach((edge) => {
+            const weight = edge[DD_NS + 's-p-o-summary/hasWeight'][0]['@value'];
+            if (weight > max) {
+                max = weight;
+            }
+        });
+        return max;
+    };
+
     _onDataLoaded = (data) => {
-        const descriptorTypeId = "http://onto.fel.cvut.cz/ontologies/dataset-descriptor/spo-summary-descriptor";
+        const descriptorTypeId = DD_NS + "spo-summary-descriptor";
 
         if (data.action === Actions.selectDatasetSource) {
             this.setState({loadedQueries: []})
@@ -75,13 +118,15 @@ class SchemaWidget extends React.Component {
                 this.props.loadingOff();
                 this.setState({
                         datasetSource: DatasetSourceStore.getSelectedDatasetSource(),
-                        datasetSchema: data.jsonLD
+                        datasetSchema: data.jsonLD,
+                        maxLimitWeight: this.computeMax(data.jsonLD)
                     }
                 );
             }
         }
     };
 
+    // creates a new node with given uri or reuses an existing if present in nodeMap.
     ensureNodeCreated(nodeMap, uri) {
         // let nodeId = mycrypto.createHash('md5').update(uri, 'UTF-8', 'UTF-8').digest().toString('base64');
         // get the node for the uri, or create a new one if the node does not exist yet
@@ -96,11 +141,18 @@ class SchemaWidget extends React.Component {
         return n;
     };
 
-    createEdge(srcNode, tgtNode, prp, fromToCount) {
+    // creates a new edge from srcNode to tgtNode using label prp.
+    // fromToCount counts number of parallel edges srcNode to tgtNode and is
+    // used to compute roundness of these edges
+    createEdge(srcNode, tgtNode, prp, fromToCount, weight) {
         const edge = JSON.parse(JSON.stringify(edgeTemplate));
         edge.from = srcNode.id;
         edge.to = tgtNode.id;
         edge.label = NamespaceStore.getShortForm(prp);
+        if ( this.state.showWeight ) {
+            edge.width = Math.round(Math.log(weight)/Math.log(5));
+            edge.title = weight;
+        }
         let count = fromToCount[srcNode.id + tgtNode.id];
         if (!count) count = 0;
         edge.smooth.roundness = getRoundnessForIthEdge(count, 10);
@@ -114,31 +166,40 @@ class SchemaWidget extends React.Component {
         const edges = [];
         const fromToCount = {};
         // transform triples to visjs nodes and edges
+        const nodesWithEdge = [];
         results.forEach(function (b) {
-            const srcNode = this.ensureNodeCreated(nodeMap, b['@id']);
-            Object.keys(b).forEach((prp) => {
-                if (prp == '@id') {
-                    return;
-                }
-                const range = b[prp][0]['@id'];
-                if (isDataType(range)) {
-                    if (this.state.showAttributes) {
-                        srcNode['label'] =
-                            srcNode['label']
-                            + "\n"
+            const srcNode = this.ensureNodeCreated(nodeMap, b[Rdf.NS+'subject'][0]['@id']);
+            const prp = b[Rdf.NS+'predicate'][0]['@id'];
+            const tgt = b[Rdf.NS+'object'][0]['@id'];
+            const weight = b[DD_NS + 's-p-o-summary/hasWeight'][0]['@value'];
+            if (isDataType(tgt)) {
+                if (this.state.showAttributes) {
+                    if (weight >= this.state.minWeight) {
+                        srcNode['label'] += "\n"
                             + NamespaceStore.getShortForm(prp)
                             + " ► "
-                            + NamespaceStore.getShortForm(range);
+                            + NamespaceStore.getShortForm(tgt);
+                        if ( this.state.showWeight ) {
+                            srcNode['label'] += " ("+weight+")";
+                        }
                     }
-                } else {
-                    const tgtNode = this.ensureNodeCreated(nodeMap, range);
-                    const edge = this.createEdge(srcNode, tgtNode, prp,fromToCount);
-                    edges.push(edge);
                 }
-            });
+            } else {
+                const tgtNode = this.ensureNodeCreated(nodeMap, tgt);
+                const edge = this.createEdge(srcNode, tgtNode, prp, fromToCount, weight);
+                if (weight >= this.state.minWeight) {
+                    edges.push(edge);
+                    nodesWithEdge.push(srcNode);
+                    nodesWithEdge.push(tgtNode);
+                }
+            }
         }.bind(this));
-        return {'nodes': Object.values(nodeMap), 'edges': edges};
+        return {'nodes': Utils.unique(nodesWithEdge), 'edges': edges};
     };
+
+    handleChange(value) {
+        this.setState({minWeight: value})
+    }
 
     render() {
         if (!this.state.datasetSource) {
@@ -151,6 +212,18 @@ class SchemaWidget extends React.Component {
                           onChange={(e) => {this.setState({showAttributes: e.target.checked});}}>
                     Show attributes
                 </Checkbox>
+                <Checkbox checked={this.state.showWeight}
+                          onChange={(e) => {this.setState({showWeight: e.target.checked});}}>
+                    Show weight
+                </Checkbox>
+                {this.state.showWeight ?
+                    <Slider
+                        min={0}
+                        max={this.state.maxLimitWeight}
+                        value={this.state.minWeight}
+                        onChange={(value) => this.handleChange(value)}
+                    />
+                 : <div/>}
                 <Graph graph={this._constructGraphData(this.state.datasetSchema)}
                        options={graphOptions}
                        style={{width: '100%', height: '400px'}}/>
@@ -158,16 +231,5 @@ class SchemaWidget extends React.Component {
         }
     };
 }
-
-// i - index of the edge
-// max - number of edges
-function getRoundnessForIthEdge(i, max) {
-    return ( ( i == 0 ) ? i : ( ( i % max ) * ( ( i % 2 ) - 0.5 ) / max ) )
-};
-
-function isDataType(uri) {
-    return uri.startsWith('http://www.w3.org/2001/XMLSchema#')
-        || uri.startsWith('http://www.w3.org/1999/02/22-rdf-syntax-ns#langString');
-};
 
 export default  LoadingWrapper(SchemaWidget, {maskClass: 'mask-container'});
