7e2ef70f8f03e321f0fe7cd786d3392f96508f9a
Petr Kremen <kremep1@fel.cvut.cz>
2018-05-25 08:48:14 +0200

    [FIX] geosparql widget to ignore invalid points

--- src/main/webapp/js/components/dashboard/widgets/spatial-widget/BoundingBox.js
+++ src/main/webapp/js/components/dashboard/widgets/spatial-widget/BoundingBox.js
@@ -4,7 +4,15 @@ export default class BoundingBox {
     min = [null,null];
     max = [null,null];
 
-    add([x, y]) {
+    add(p) {
+        if (!p || !p[0] || !p[1]) {
+            console.log("Invalid point : " + p);
+            return;
+        }
+
+        const x = p[0];
+        const y = p[1];
+
         if ((this.min[0] === null) || (x < this.min[0])) this.min[0] = x;
         if ((this.min[1] === null) || (y < this.min[1])) this.min[1] = y;
         if ((this.max[0] === null) || (x > this.max[0])) this.max[0] = x;
